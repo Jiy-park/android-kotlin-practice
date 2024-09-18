bc0869d9a6da18bdac221219aa3294ee6c9ec61b
Jiy-park <jiyong3954@gmail.com>
2024-09-18 20:03:08 +0900

    case 2 - 기존 앱에 스플래시 api 추가

--- splash/src/main/java/com/dd2d/splash/MainActivity.kt
+++ splash/src/main/java/com/dd2d/splash/MainActivity.kt
@@ -9,13 +9,25 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import com.dd2d.splash.ui.theme.KotlinpracticeTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+//        이건 없어도 됨
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+
         enableEdgeToEdge()
         setContent {
             KotlinpracticeTheme {
